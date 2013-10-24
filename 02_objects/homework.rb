a84ca0495cb46d8ee7801d2f735de35fd35c915f
Kim Balin <kimika8@gmail.com>
2013-10-24 13:28:41 -0400

    Finished homework.rb October 24

--- 02_objects/homework.rb
+++ 02_objects/homework.rb
@@ -1,3 +1,21 @@
+class Person
+  def initialize name, profession
+    @name = name
+    @profession = profession
+  end
+  
+  attr_accessor :name
+  attr_accessor :profession
+
+  def introduce
+    "Hi! My name is #{@name}. I'm a #{@profession}"
+  end
+
+  def introduce_from_across_the_room
+    "HI! MY NAME IS #{@name.upcase}. I'M A #{@profession.upcase}"
+  end
+end
+
 require 'minitest/autorun'
 
 describe Person do 
