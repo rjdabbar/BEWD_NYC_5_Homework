ce1778216fd5289d9f01de810d09920187d600c2
Dan Schanler <hello@danschanler.com>
2013-10-28 16:18:28 -0400

    homework done, finally

---
+++ Dan_Schanler/homework_extra.rb
@@ -0,0 +1,45 @@
+require 'minitest/autorun'
+
+class Story
+  def initialize title, category
+    @title = title
+    @category = category
+    @upvotes = 1
+  end
+
+  attr_reader :title, :category 
+  attr_accessor :upvotes
+
+  def upvote
+    @upvotes = @upvotes + 1
+  end
+
+  def downvote
+    @upvotes = @upvotes - 1
+  end
+end
+
+
+
+### Write your code above the tests ###
+describe Story do 
+  it "requires a title and a category on initialization" do
+    story = Story.new "Wombat sees dentist after 10 year hiatus", "Nature"
+    story.title.must_equal "Wombat sees dentist after 10 year hiatus"
+    story.category.must_equal "Nature"
+  end
+
+  it "has its upvotes set to 1 by default" do
+    story = Story.new "Wombat sees dentist after 10 year hiatus", "Nature"
+    story.upvotes.must_equal  1
+  end
+
+  it "can be upvoted and downvoted" do
+    story = Story.new "Wombat sees dentist after 10 year hiatus", "Nature"
+    story.upvote
+    story.upvotes.must_equal 2
+
+    story.downvote
+    story.upvotes.must_equal 1
+  end
+end
